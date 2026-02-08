41e9814fede316b541b7899eddbb18f20976fedb
frontend-alpinads <fath@alpinads.com>
2026-02-08 15:27:56 +0100

    Populate hotelConfig with scraped data

--- hotelConfig.ts
+++ hotelConfig.ts
@@ -1,7 +1,7 @@
 export const hotelConfig = {
-  hotelName: "{{HOTEL_NAME}}",
-  email: "{{EMAIL}}",
-  address: "{{ADDRESS}}",
-  phoneNumber: "{{PHONE_NUMBER}}",
-  websiteUrl: "{{WEBSITE_URL}}",
+  hotelName: "STROBLHOF LAKE GARDA Active Family SPA Resort",
+  email: "",
+  address: "Gargnano am Gardasee",
+  phoneNumber: "+39 0365 1702700",
+  websiteUrl: "https://www.stroblhof-gardasee.com/",
 };
